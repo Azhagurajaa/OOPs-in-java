9b5194dadb6e5292db3ed64cefccfbd3d5aba825
Azhagurajaa <89646803+Azhagurajaa@users.noreply.github.com>
2023-11-05 06:39:49 -0800

    oops

---
+++ methodReturntype.java
@@ -0,0 +1,21 @@
+public class methodReturntype
+{
+	public static void main(String []args)
+	{
+		
+        System.out.println(add(20,20));		
+		System.out.println("add method is printed");
+	}
+
+        /*
+		   with perameter method
+		   return type
+		*/
+
+
+   	public static int add(int a,int b)
+	{
+		int c=a+b;
+		return c;
+	}
+}
---
+++ methodWithperameter.java
@@ -0,0 +1,20 @@
+public class methodWithperameter
+{
+	public static void main(String []args)
+	{
+		add(20,20); 
+		System.out.println("add method is printed");
+	}
+
+        /*
+		   with perameter method
+		   no return type
+		*/
+
+
+   	public static void add(int a,int b)
+	{
+		int c=a+b;
+		System.out.println(c);
+	}
+}
