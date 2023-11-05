8a2b5f6d3de4365e53a39389aa306bc73e583915
Azhagurajaa <89646803+Azhagurajaa@users.noreply.github.com>
2023-11-05 06:37:12 -0800

    oops
    method

---
+++ method.java
@@ -0,0 +1,21 @@
+public class method
+{
+	public static void main(String []args)
+	{
+      add(); //without perameter
+	  System.out.println("print add method");
+	}
+           /*
+		      without perameter
+			  no return type method & void method
+		   */
+  
+  
+    public static void add()//no return type method
+	{
+		int a=20;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+	}
+}	
